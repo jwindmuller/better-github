cf65a84d375b0350a9dac004c5a06b7f8196b79e
Joaquin Windmuller <joaquin@rpmsoftware.com>
2024-12-02 09:53:16 -0700

    Update code to support new issue UI
    Kept support for the old one, will be removed later.

--- index.js
+++ index.js
@@ -38,17 +38,36 @@ const debouncedActionOnDOMChange = (element, action) => {
 }
 
 // Extension code
-const hideCommitEntries = async (parentContainer) => {
-    if (parentContainer === undefined) {
-        parentContainer = document;
-    }
-    const commitHistoryEntries = Array.from(parentContainer.querySelectorAll('.TimelineItem [id^=ref-commit]'));
-    commitHistoryEntries.forEach(commitEntry => {
-        const container = commitEntry.closest('.TimelineItem');
+const hideCommitEntries = async () => {
+    const commitHistoryEntries = getCommitHistoryEntriesContainers();
+    commitHistoryEntries.forEach(container => {
         container.style.display = 'none'
     });
 
-    document.querySelector('.gh-header-title').scrollIntoView({behavior:'smooth', block: 'center', inline: 'center'});
+    let header = document.querySelector('.gh-header-title');
+    if (!header) { 
+        header = document.querySelector('[data-testid="issue-title"]');
+    }
+    setTimeout(() => {
+        header.scrollIntoView({
+            behavior:'smooth',
+            block: 'center',
+            inline: 'center'
+        });
+    }, 100);
+};
+
+const getCommitHistoryEntriesContainers = () => {
+    let commitElementsToHide = Array.from(document.querySelectorAll('.TimelineItem [id^=ref-commit]'));
+    
+    if (commitElementsToHide.length > 0) {
+        return commitElementsToHide;
+    }
+    commitElementsToHide =Array.from(document.querySelectorAll('[data-hovercard-url*=commit]')) ;
+    commitElementsToHide = commitElementsToHide.map(commit => commit.closest('[data-timeline-event-id]'));
+    commitElementsToHide = commitElementsToHide
+        .filter((timelineItemsContainer, index) => commitElementsToHide.indexOf(timelineItemsContainer) !== index);
+    return commitElementsToHide;
 };
 
 const getBlanket = () => { 
@@ -66,7 +85,7 @@ const getBlanket = () => {
 const expandLoadMore = (onComplete) => {
     
     getBlanket().classList.add('shown');
-    const loadMoreButton = document.querySelector('.ajax-pagination-btn');
+    const loadMoreButton = getLoadMoreButton();
     if (!loadMoreButton) {
         getBlanket().classList.remove('shown');
         if (onComplete) {
@@ -75,27 +94,47 @@ const expandLoadMore = (onComplete) => {
         return;
     }
 
-    let container = loadMoreButton.closest('#js-progressive-timeline-item-container');
-    if (container === null) {
-        container = loadMoreButton.closest('.TimelineItem-body');
-    }
-    debug('START LOADING', container);
+    let containers = getMoreContentContainers(loadMoreButton);
+    debug({loadMoreButton,container: containers})
+    debug('START LOADING', containers);
     loadMoreButton.scrollIntoView(false, { behavior: 'instant', block: 'center', inline: 'center' });
     window.scrollTo({ top: window.scrollY + 100, left: 0 });
     loadMoreButton.click();
+    for (const container of containers) {
+        debouncedActionOnDOMChange(container, () => {
+            expandLoadMore(onComplete)
+        });
+    }
+}
 
-    debouncedActionOnDOMChange(container, () => {
-        expandLoadMore(onComplete)
-    });
+const getLoadMoreButton = () => { 
+    let button = document.querySelector('[data-testid="issue-timeline-load-more-load-top"]');
+    if (button) {
+        return button;
+    }
+    button = document.querySelector('.ajax-pagination-btn');
+    return button;
+}
+
+const getMoreContentContainers = (button) => {
+    const isOldButton = button.classList.contains('ajax-pagination-btn');
+    if (isOldButton) {
+        let container = button.closest('#js-progressive-timeline-item-container');
+        if (container === null) {
+            container = button.closest('.TimelineItem-body');
+        }
+        return [container];
+    }
+    const eventContainers = document.querySelectorAll('[data-testid="issue-timeline-container"] [aria-label="Events"]');
+
+    return Array.from(eventContainers);
 }
 
 const findCommentsStartingWith = (text) => {
     let nextCommentToFocus = 0;
     return () => {
         const searchTerm = text.toLowerCase();
-        const comments = Array.from(
-            document.querySelectorAll('.TimelineItem.js-comment-container task-lists')
-        );
+        const comments = getCommentElements();
         const found = comments.reduce((commentsThatMatch, comment, currentIndex) => { 
             const commentText = comment.innerText.toLowerCase().trim();
             const isMatch = commentText.indexOf(searchTerm) === 0;
@@ -124,6 +163,21 @@ const findCommentsStartingWith = (text) => {
         }
     }
 };
+
+const getCommentElements = () => {
+    let comments = Array.from(
+        document.querySelectorAll('.TimelineItem.js-comment-container task-lists')
+    );
+    console.log({comments})
+    if (comments.length > 0) {
+        return comments;
+    }
+    comments = Array.from(
+        document.querySelectorAll('[data-testid^=comment-viewer-outer-box] [data-testid=markdown-body]')
+    );
+    console.log({comments})
+    return comments;
+}
 
 const client =  chrome ? chrome : browser;
 const findFunctions = {};
